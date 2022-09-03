1a78b8b9bc24f0c9b5a4d5e234102d4b5cf0f6e1
Chipskein <abfn0905@gmail.com>
2022-09-03 02:27:48 -0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.a4cb0ce0e9bdf0c19aa7e38d147e80a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d03cb0313bf2204c8628",
+    "url": "/weather-R/static/css/main.55ea049d.chunk.css"
+  },
+  {
+    "revision": "d03cb0313bf2204c8628",
+    "url": "/weather-R/static/js/main.d03cb031.chunk.js"
+  },
+  {
+    "revision": "edaeb1e94a6c4bdf8a44",
+    "url": "/weather-R/static/js/1.edaeb1e9.chunk.js"
+  },
+  {
+    "revision": "aec98957a76b1f6e102f",
+    "url": "/weather-R/static/js/runtime~main.aec98957.js"
+  },
+  {
+    "revision": "93ad17a0dc534d721a9436a2f027a518",
+    "url": "/weather-R/static/media/icon.93ad17a0.png"
+  },
+  {
+    "revision": "eb1aef8044b76a5de706aedc8cf4f00a",
+    "url": "/weather-R/index.html"
+  }
+];
